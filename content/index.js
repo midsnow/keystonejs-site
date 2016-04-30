94208606849759241c43ac650bcd02f24930fd27
Bret Newman <snowkeeper@snowpi.org>
2016-04-29 21:31:26 -0400

    i18n initial

--- content/index.js
+++ content/index.js
@@ -51,31 +51,31 @@ module.exports = {
 		}
 	},
 	
-	"routes": [
+	"routes": {
 		
 		// Language: en
-		{
+		'/': {
 			"path": "/",
 			"language": "en",
 			"template": "home",
 			"section": "home",
 			"title": "Node.js cms and web application platform built on Express and MongoDB"
 		},
-		{
+		"/getting-started": {
 			"path": "/getting-started",
 			"language": "en",
 			"template": "starting",
 			"section": "starting",
 			"title": "Getting started"
 		},
-		{
+		"/examples": {
 			"path": "/examples",
 			"language": "en",
 			"template": "examples",
 			"section": "examples",
 			"title": "Sample applications"
 		},
-		{
+		"/docs": {
 			"path": "/docs",
 			"language": "en",
 			"template": "docs/index",
@@ -83,7 +83,7 @@ module.exports = {
 			"docssection": { "value": "introduction", "path": "/docs", "label": "Introduction" },
 			"title": "KeystoneJS Documentation"
 		},
-		{
+		"/docs/getting-started": {
 			"path": "/docs/getting-started",
 			"language": "en",
 			"template": "docs/getting-started",
@@ -91,7 +91,7 @@ module.exports = {
 			"docssection": { "value": "getting-started", "path": "/docs/guides", "label": "Guides" },
 			"title": "Getting Started"
 		},
-		{
+		"/docs/configuration": {
 			"path": "/docs/configuration",
 			"language": "en",
 			"template": "docs/configuration",
@@ -99,7 +99,7 @@ module.exports = {
 			"docssection": { "value": "configuration", "path": "/docs/configuration", "label": "Configuration" },
 			"title": "Configuration"
 		},
-		{
+		"/docs/database": {
 			"path": "/docs/database",
 			"language": "en",
 			"template": "docs/database",
@@ -107,148 +107,7 @@ module.exports = {
 			"docssection": { "value": "database", "path": "/docs/database", "label": "Database" },
 			"title": "Setting up and using Data Models"
 		},
-		{
-			"path": "/docs/api",
-			"language": "en",
-			"template": "docs/api",
-			"section": "docs",
-			"docssection": { "value": "api", "path": "/docs/api", "label": "Keystone Api" },
-			"title": "Keystone Api"
-		},
-		
-		{
-			"path": "/docs/api/current",
-			"language": "en",
-			"template": "docs/api/0.3.x/class",
-			"section": "docs",
-			"api": '0.3.x',
-			"branch": 'v0.3.x',
-			"_version": "current",
-			"docssection": { "version": "0.3.x", "value": "classapi", "path": "/docs/api/current/class", "label": "Class Reference" },
-			"title": "Class Reference"
-		},
-		{
-			"path": "/docs/api/current/class",
-			"language": "en",
-			"template": "docs/api/0.3.x/class",
-			"section": "docs",
-			"api": '0.3.x',
-			"branch": 'v0.3.x',
-			"_version": "current",
-			"docssection": { "version": "0.3.x", "value": "classapi", "path": "/docs/api/current/class", "label": "Class Reference" },
-			"title": "Class Reference"
-		},
-		{
-			"path": "/docs/api/incoming",
-			"language": "en",
-			"template": "docs/api/0.4.x/class",
-			"section": "docs",
-			"api": '0.4.x',
-			"_version": "incoming",
-			"branch": 'master',
-			"docssection": { "version": "0.4.x", "value": "classapi", "path": "/docs/api/incoming/class", "label": "Class Reference" },
-			"title": "Class Reference"
-		},
-		{
-			"path": "/docs/api/incoming/class",
-			"language": "en",
-			"template": "docs/api/0.4.x/class",
-			"section": "docs",
-			"api": '0.4.x',
-			"branch": 'master',
-			"_version": "incoming",
-			"docssection": { "version": "0.4.x", "value": "classapi", "path": "/docs/api/incoming/class", "label": "Class Reference" },
-			"title": "Class Reference"
-		},
-		{
-			"path": "/docs/api/current/endpoint",
-			"language": "en",
-			"template": "docs/api/0.4.x/endpoint",
-			"section": "docs",
-			"api": '0.4.x',
-			"branch": 'master',
-			"_version": "incoming",
-			"docssection": { "version": "0.4.x", "value": "endpointapi", "path": "/docs/api/current/class", "label": "Class Api" },
-			"title": "Class Api"
-		},
-		{
-			"path": "/docs/api/0.4.x",
-			"language": "en",
-			"template": "docs/api/0.4.x/class",
-			"section": "docs",
-			"api": '0.4.x',
-			"branch": 'master',
-			"_version": "incoming",
-			"docssection": { "version": "0.4.x", "value": "classapi", "path": "/docs/api/0.4.x/class", "label": "Keystone Class Reference" },
-			"title": "Keystone Class Reference"
-		},
-		{
-			"path": "/docs/api/0.4.x/class",
-			"language": "en",
-			"template": "docs/api/0.4.x/class",
-			"section": "docs",
-			"api": '0.4.x',
-			"branch": 'master',
-			"_version": "incoming",
-			"docssection": { "version": "0.4.x", "value": "classapi", "path": "/docs/api/0.4.x/class", "label": "Keystone Class Reference" },
-			"title": "Keystone Class Reference"
-		},
-		{
-			"path": "/docs/api/0.4.x/endpoint",
-			"language": "en",
-			"template": "docs/api/0.4.x/endpoint",
-			"section": "docs",
-			"api": '0.4.x',
-			"branch": 'master',
-			"_version": "incoming",
-			"docssection": { "version": "0.4.x", "value": "endpointapi", "path": "/docs/api/current/endpoint", "label": "Class Reference" },
-			"title": "Class Reference"
-		},
-		{
-			"path": "/docs/api/0.3.x",
-			"language": "en",
-			"template": "docs/api/0.3.x/class",
-			"section": "docs",
-			"api": '0.3.x',
-			"branch": 'v0.3.x',
-			"_version": "current",
-			"docssection": { "version": "0.3.x", "value": "classapi", "path": "/docs/api", "label": "Keystone Class Reference" },
-			"title": "Keystone Class Reference"
-		},
-		{
-			"path": "/docs/api/0.3.x/class",
-			"language": "en",
-			"template": "docs/api/0.3.x/class",
-			"section": "docs",
-			"api": '0.3.x',
-			"branch": 'v0.3.x',
-			"_version": "current",
-			"docssection": { "version": "0.3.x", "value": "classapi", "path": "/docs/api", "label": "Keystone Class Reference" },
-			"title": "Keystone Class Reference"
-		},
-		{
-			"path": "/docs/api/0.2.x",
-			"language": "en",
-			"template": "docs/api/0.2.x/class",
-			"section": "docs",
-			"api": '0.2.x',
-			"branch": '0.2.x',
-			"_version": "0.2.x",
-			"docssection": { "version": "0.2.x", "value": "classapi", "path": "/docs/api", "label": "Keystone Class Reference" },
-			"title": "Keystone Class Reference"
-		},
-		{
-			"path": "/docs/api/0.2.x/class",
-			"language": "en",
-			"template": "docs/api/0.2.x/class",
-			"section": "docs",
-			"api": '0.2.x',
-			"branch": '0.2.x',
-			"_version": "0.2.x",
-			"docssection": { "version": "0.2.x", "value": "classapi", "path": "/docs/api/0.2.x/class", "label": "Class Reference 0.2.x" },
-			"title": "Class Reference 0.2.x"
-		},
-		{
+		"/docs/faqs": {
 			"path": "/docs/faqs",
 			"language": "en",
 			"template": "docs/faqs",
@@ -256,75 +115,19 @@ module.exports = {
 			"docssection": { "value": "faqs", "path": "/docs/faqs", "label": "FAQs" },
 			"title": "Frequently Asked Questions"
 		},
-		/* {
-			"path": "/docs/views",
-			"template": "docs/views",
+		//
+		// DEVELOPMENT SERVER ONLY
+		"/docs/api": {
+			"path": "/docs/api",
+			"language": "en",
+			"template": "docs/api",
 			"section": "docs",
-			"docssection": { "value": "views", "path": "/docs/views", "label": "Views" },
-			"title": "Using Views in your application"
-		},*/
+			"docssection": { "value": "api", "path": "/docs/api", "label": "Keystone Api" },
+			"title": "Keystone Api",
+			"development": "true",
+			"versions": ['0.2.x','0.3.x','0.4.x'],
+			"current": '0.3.x'
+		},
 		
-		// Language: zh
-		{
-			"path": "/zh",
-			"language": "zh",
-			"template": "home",
-			"section": "home",
-			"title": "基于Express和MongoDB搭建的Node.js cms和web应用程序平台"
-		},
-		{
-			"path": "/zh/getting-started",
-			"language": "zh",
-			"template": "starting",
-			"section": "starting",
-			"title": "入门"
-		},
-		{
-			"path": "/zh/examples",
-			"language": "zh",
-			"template": "examples",
-			"section": "examples",
-			"title": "范例程序"
-		},
-		{
-			"path": "/zh/docs",
-			"language": "zh",
-			"template": "docs/index",
-			"section": "docs",
-			"docssection": { "value": "introduction", "path": "/zh/docs", "label": "Introduction" },
-			"title": "KeystoneJS文档"
-		},
-		{
-			"path": "/zh/docs/getting-started",
-			"language": "zh",
-			"template": "docs/getting-started",
-			"section": "docs",
-			"docssection": { "value": "getting-started", "path": "/zh/docs/guides", "label": "Guides" },
-			"title": "入门"
-		},
-		{
-			"path": "/zh/docs/configuration",
-			"language": "zh",
-			"template": "docs/configuration",
-			"section": "docs",
-			"docssection": { "value": "configuration", "path": "/zh/docs/configuration", "label": "Configuration" },
-			"title": "配置"
-		},
-		{
-			"path": "/zh/docs/database",
-			"language": "zh",
-			"template": "docs/database",
-			"section": "docs",
-			"docssection": { "value": "database", "path": "/zh/docs/database", "label": "Database" },
-			"title": "设置和使用数据模型"
-		},
-		/* {
-			"path": "/zh/docs/views",
-			"language": "zh",
-			"template": "docs/views",
-			"section": "docs",
-			"docssection": { "value": "views", "path": "//zhdocs/views", "label": "Views" },
-			"title": "在你的应用程序中使用Views"
-		},*/
-	]
+	}
 }
--- index.js
+++ index.js
@@ -2,43 +2,40 @@
  * This script launches the KeystoneJS Docs website using express.
  */
 
+var production = (process.env.NODE_ENV === 'production');
 var _ = require('lodash');
 var errorHandler = require('errorhandler');
 var express = require('express');
 var favicon = require('serve-favicon');
 var less = require('less-middleware');
 var logger = require('morgan');
+var i18n = require('i18n');
+var cookieParser = require('cookie-parser');
 
 var content = require('./content');
 
-var classReference = require('./content/common/apiMarked');
-//var marked = _.cloneDeep(classReference._marked);
-//classReference.getApi(marked);
+//var classReference = require('./content/common/apiMarked');
+var defaultLocale = 'en';
+var languages = [defaultLocale, 'zh', 'es'];
 
-function view(view, options) {
-	return function(req, res, next) {
-		options.pretty = true;
-		delete options.apidocs;
-		options.prefix = (options.language === 'en') ? '/' : '/' + options.language + '/';
-		_.extend(options, content.languages[options.language]);
-		if(_.contains(['0.2.x','0.3.x','0.4.x'], options.api)) {
-			var sendMarked = _.cloneDeep(classReference._marked[options.language][options.api]);
-			classReference.getApi(sendMarked, options.language, options.api, function(err, marked) {
-				options.apidocs = sendMarked;
-				res.render(options.language + '/pages/' + view, options);
-			});
-			
-		} else {
-			res.render(options.language + '/pages/' + view, options);
-		}
-	}
-}
+i18n.configure({
+	locales: languages,
+	// language json file path
+	directory: __dirname + '/locales',
+	// default language
+	defaultLocale: defaultLocale,
+	// define a custom cookie name to parse locale settings from 
+	cookie: 'i18n'
+});
 
 var app = express();
 
 app.set('port', process.env.PORT || 8080);
 app.set('views', 'content');
 app.set('view engine', 'jade');
+
+app.use(cookieParser("i18n"));
+app.use(i18n.init);
 
 app.use(favicon('public/favicon.ico'));
 app.use(less('public'));
@@ -59,8 +56,43 @@ app.use(function(req, res, next) {
 app.locals.languages = content.languages;
 app.locals.version = require('./package.json').version;
 
-_.each(content.routes, function(options) {
-	app.get(options.path, view(options.template, options));
+languages.forEach(function(lang) {
+	app.get('/' + lang + '/:uri1?/:uri2?', function (req, res) {
+		console.log('set cookie', lang);
+		res.cookie('i18n', lang);
+		var link = '/' + req.params.uri1;
+		if(req.params.uri2) {
+			link += '/' + req.params.uri2;
+		}
+		res.redirect(link);
+	});
+});
+
+app.get('/:uri1?/:uri2?', function (req, res) {
+	if(!req.cookies.i18n) {
+		console.log('set cookie', defaultLocale);
+		res.cookie('i18n', defaultLocale);
+		res.setLocale(defaultLocale);
+	}
+	var options = false;
+	var link = '/';
+	if(req.params.uri1) {
+		link += req.params.uri1;
+	}
+	if(req.params.uri2) {
+		link += '/' + req.params.uri2;
+	}
+	console.log(link);
+	options = content.routes[link];
+	if(options) {
+		// load jade template
+		console.log('set locale', req.cookies.i18n);
+		res.setLocale(req.cookies.i18n);
+		view(options.template, options, res, req.cookies.i18n)
+	} else {
+		// 404
+		res.status(404).render('en/pages/404');
+	}
 });
 
 app.use(function(req, res, next) {
@@ -70,7 +102,14 @@ app.use(function(req, res, next) {
 app.use(errorHandler());
 
 // Start server
-
 app.listen(app.get('port'), function() {
   console.log('Keystone docs are available on port ' + app.get('port'));
 });
+
+function view(view, options, res, lang) {
+	options.pretty = true;
+	_.extend(options, content.languages[lang]);
+	options.__ = res.__;
+	options.language = lang;	
+	res.render('en/pages/' + view, options);
+}
